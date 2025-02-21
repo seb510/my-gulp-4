c7d545fc6d3bdc2726514878fd761efff10ad094
shurshyn-sergey <123107680+shurshyn-sergey@users.noreply.github.com>
2025-02-21 08:22:43 +0200

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,219 +1,174 @@
 "use strict"
 
-const {src, dest} = require("gulp")
+const { src, dest, series, parallel, watch } = require("gulp")
 const gulp = require("gulp")
 const autoprefixer = require("gulp-autoprefixer")
-const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require('gulp-strip-css-comments');
-const rename = require("gulp-rename");
-const rigger = require("gulp-rigger");
-const concat = require('gulp-concat');
-const sass = require("gulp-sass")(require('sass'));
-const cssnano = require("gulp-cssnano");
-const uglify = require("gulp-uglify");
-const plumber = require("gulp-plumber");
-const panini = require("panini");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
+const cssbeautify = require("gulp-cssbeautify")
+const removeComments = require("gulp-strip-css-comments")
+const rename = require("gulp-rename")
+const concat = require("gulp-concat")
+const sass = require("gulp-sass")(require("sass"))
+const cssnano = require("gulp-cssnano")
+const uglify = require("gulp-uglify")
+const plumber = require("gulp-plumber")
+const panini = require("panini")
+const imagemin = require("gulp-imagemin")
+const del = require("del")
 const notify = require("gulp-notify")
 const imagewebp = require("gulp-webp")
-const browserSync = require("browser-sync").create();
+const browserSync = require("browser-sync").create()
 
 /* Paths */
 const srcPath = "src/"
 const distPath = "dist/"
+const assetsPath = "assets/"
 
 const path = {
     build: {
         html: distPath,
-        css: distPath + "assets/css/",
-        cssLibs: distPath + "assets/css/",
-        js: distPath + "assets/js/",
-        jsLibs: distPath + "assets/js/",
-        images: distPath + "assets/img/",
-        fonts: distPath + "assets/fonts/"
+        css: `${distPath}${assetsPath}css/`,
+        js: `${distPath}${assetsPath}js/`,
+        images: `${distPath}${assetsPath}images/`,
+        fonts: `${distPath}${assetsPath}fonts/`
     },
     src: {
-        html: srcPath + "*.html",
-        css: srcPath + "assets/scss/**/*.scss",
-        cssLibs: srcPath + "assets/css/libs/*.css",
-        js: srcPath + "assets/js/*.js",
-        jsLibs: srcPath + "assets/js/libs/*.js",
-        images: srcPath + "assets/img/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
-        fonts:  srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}"
+        html: `${srcPath}*.html`,
+        css: `${srcPath}${assetsPath}scss/**/*.scss`,
+        cssLibs: `${srcPath}${assetsPath}css/libs/*.css`,
+        js: `${srcPath}${assetsPath}js/*.js`,
+        jsLibs: `${srcPath}${assetsPath}js/libs/*.js`,
+        images: `${srcPath}${assetsPath}img/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}`,
+        fonts: `${srcPath}${assetsPath}fonts/**/*.{eot,woff,woff2,ttf,svg}`
     },
     watch: {
-        html:   srcPath + "**/*.html",
-        js:     srcPath + "assets/js/*.js",
-        jsLibs: srcPath + "assets/js/*.js",
-        css:    srcPath + "assets/scss/**/*.scss",
-        cssLibs: srcPath + "assets/css/libs/*.css",
-        images: srcPath + "assets/img/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
-        fonts:  srcPath + "assets/fonts/**/*.{eot,woff,woff2,ttf,svg}"
+        html: `${srcPath}**/*.html`,
+        css: `${srcPath}${assetsPath}scss/**/*.scss`,
+        js: `${srcPath}${assetsPath}js/*.js`,
+        images: `${srcPath}${assetsPath}images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}`,
+        fonts: `${srcPath}${assetsPath}fonts/**/*.{eot,woff,woff2,ttf,svg}`
     },
-    clean: "./" + distPath
+    clean: `./${distPath}`
 }
 
+/* Browser Sync */
 function serve() {
     browserSync.init({
-        server: {
-            baseDir: "./" + distPath
-        }
-    });
+        server: { baseDir: `./${distPath}` }
+    })
 }
 
-
+/* HTML */
 function html() {
     panini.refresh()
-    return src(path.src.html, {base: srcPath})
-        .pipe(plumber())
+    return src(path.src.html)
+        .pipe(plumber({ errorHandler: notify.onError("HTML Error: <%= error.message %>") }))
         .pipe(panini({
             root: srcPath,
-            layouts: srcPath + "tpl/layouts/",
-            partials: srcPath + "tpl/partials/",
-            data: srcPath + "tpl/data/"
-          }))
+            layouts: `${srcPath}tpl/layouts/`,
+            partials: `${srcPath}tpl/partials/`,
+            data: `${srcPath}tpl/data/`
+        }))
         .pipe(dest(path.build.html))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream())
 }
 
+/* CSS */
 function css() {
-    return src(path.src.css, {base: srcPath + "assets/scss/"})
-        .pipe(plumber({
-            errorHandler : function(err) {
-                notify.onError({
-                    title:    "SCSS Error",
-                    message:  "Error: <%= error.message %>"
-                })(err);
-                this.emit('end');
-            }
-        }))
+    return src(path.src.css)
+        .pipe(plumber({ errorHandler: notify.onError("SCSS Error: <%= error.message %>") }))
         .pipe(sass())
-        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+        .pipe(autoprefixer({ overrideBrowserslist: ["last 10 versions"], grid: true }))
         .pipe(cssbeautify())
         .pipe(dest(path.build.css))
-        .pipe(cssnano({
-            zindex: false,
-            discardComments: {
-                removeAll: true
-            }
-        }))
+        .pipe(cssnano({ zindex: false, discardComments: { removeAll: true } }))
         .pipe(removeComments())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".css"
-        }))
+        .pipe(rename({ suffix: ".min" }))
         .pipe(dest(path.build.css))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream())
 }
 
-//Library
+/* CSS Libraries */
 function cssLibs() {
-    return src([
-        'src/assets/css/libs/**/*.css',
-        'src/assets/css/libs/**/*.min.css',
-    ])
-        .pipe(concat('libs.css'))
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".css"
-        }))
-        .pipe(dest(path.build.cssLibs))
-        .pipe(browserSync.reload({stream: true}));
+    return src([path.src.cssLibs])
+        .pipe(concat("libs.min.css"))
+        .pipe(dest(path.build.css))
+        .pipe(browserSync.stream())
 }
 
+/* JavaScript */
 function js() {
-    return src(path.src.js, {base: srcPath + "assets/js/"})
-        .pipe(plumber({
-            errorHandler : function(err) {
-                notify.onError({
-                    title:    "JS Error",
-                    message:  "Error: <%= error.message %>"
-                })(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(rigger())
+    return src(path.src.js)
+        .pipe(plumber({ errorHandler: notify.onError("JS Error: <%= error.message %>") }))
         .pipe(dest(path.build.js))
         .pipe(uglify())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".js"
-        }))
+        .pipe(rename({ suffix: ".min" }))
         .pipe(dest(path.build.js))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream())
 }
 
+/* JavaScript Libraries */
 function jsLibs() {
-    return src([
-        'src/assets/js/libs/**/*.js',
-        'src/assets/js/libs/**/*.min.js',
-    ])
-        .pipe(concat('libs.js'))
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".js"
-        }))
+    return src([path.src.jsLibs])
+        .pipe(concat("libs.min.js"))
         .pipe(uglify())
-        .pipe(dest(path.build.jsLibs))
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(dest(path.build.js))
+        .pipe(browserSync.stream())
 }
 
+/* Images */
 function images() {
-    return src(path.src.images, {base: srcPath + "assets/img/"})
+    return src(path.src.images)
         .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            //imagemin.mozjpeg({quality: 75, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({ plugins: [{ removeViewBox: true }, { cleanupIDs: false }] })
         ]))
         .pipe(dest(path.build.images))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream())
 }
 
+/* WebP Images */
 function webpImages() {
-    return src(path.src.images, {base: srcPath + "assets/img/"})
+    return src(path.src.images)
         .pipe(imagewebp())
         .pipe(dest(path.build.images))
 }
 
+/* Fonts */
 function fonts() {
-    return src(path.src.fonts, {base: srcPath + "assets/fonts/"})
-    .pipe(dest(path.build.fonts))
-    .pipe(browserSync.reload({stream: true}));
+    return src(path.src.fonts)
+        .pipe(dest(path.build.fonts))
+        .pipe(browserSync.stream())
 }
 
+/* Clean */
 function clean() {
     return del(path.clean)
 }
 
+/* Watch */
 function watchFiles() {
-    gulp.watch([path.watch.html], html)
-    gulp.watch([path.watch.cssLibs], cssLibs)
-    gulp.watch([path.watch.css], css)
-    gulp.watch([path.watch.jsLibs], jsLibs)
-    gulp.watch([path.watch.js], js)
-    gulp.watch([path.watch.images], images)
-    gulp.watch([path.watch.fonts], fonts)
+    watch(path.watch.html, html)
+    watch(path.watch.css, css)
+    watch(path.watch.js, js)
+    watch(path.watch.images, images)
+    watch(path.watch.fonts, fonts)
+    serve()
 }
 
-const build = gulp.series(clean, gulp.parallel(html, cssLibs, css, jsLibs, js, images, webpImages, fonts))
-const watch = gulp.parallel(build, watchFiles, serve)
+/* Tasks */
+const build = series(clean, parallel(html, cssLibs, css, jsLibs, js, images, webpImages, fonts))
+const dev = parallel(build, watchFiles)
 
 exports.html = html
-exports.csslibs = cssLibs
 exports.css = css
-exports.jslibs = jsLibs
+exports.cssLibs = cssLibs
 exports.js = js
+exports.jsLibs = jsLibs
 exports.images = images
 exports.webpImages = webpImages
 exports.fonts = fonts
 exports.clean = clean
 exports.build = build
-exports.watch = watch
-exports.default = watch
+exports.dev = dev
+exports.default = dev
